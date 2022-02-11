6fc0eae15d4cbe054aa698d96e89ce5f660874f1
Abhi <programwithabhi@gmail.com>
2022-02-11 22:29:53 +0530

    Second Commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 export default function TextForm(props) {
+    // the first argument is the acutal value {text} and the second {nextTet}argument is the function that
+    // you can use to mutate the text variable
+    const [text,nextText] = useState("Enter the text here")
+    const onClickUpHandler = ()=>{
+        const newText = text.toUpperCase()
+        nextText(newText)
+        console.log("Clicked on the onclick handler")
+        // nextText("You have been clicked on onClickHandler")
+    }
+    const onClickLowHandler = ()=>{
+        const newText = text.toLowerCase()
+        nextText(newText)
+        console.log("Clicked on the onclick handler")
+        // nextText("You have been clicked on onClickHandler")
+    }
+    const onChangeHandler = (event)=>{
+        console.log("This is a onChangeHandler")
+        nextText(event.target.value)
+    }
+
     return (
         <div className="container my-4">
             <h3>{props.heading}</h3>
-            <textarea className="form-control" placeholder="Leave a text here" id="floatingTextarea2" rows="8"></textarea>
+            <textarea value={text} onChange={onChangeHandler} className="form-control" id="floatingTextarea2" rows="8"></textarea>
             <div className="container">
-            <button className="btn btn-primary my-3 mx-auto d-block">Submit</button>
+            <button onClick={onClickUpHandler} className="mx-2 btn btn-primary my-3">ChangeToUpperCase</button>
+            <button onClick={onClickLowHandler} className="mx-2 btn btn-primary my-3 ">ChangeToLowerCase</button>
             </div>
         </div>
     );
